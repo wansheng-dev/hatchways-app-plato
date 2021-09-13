2bdc3138718204d5327184bd34046a0ab48d3972
Wan <wanshengdev@gmail.com>
2021-09-12 22:49:22 -0700

    part 2 finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			const res = await axios.get(
+				"https://api.hatchways.io/assessment/students"
+			);
+			setData(res.data.students);
+			setLoading(false);
+		};
+		fetchData();
+	}, []);
+
+	console.log(data);
+
+	return (
+		<div className="wrapper">
+			<div className="content mx-auto">
+				{loading ? (
+					<h2>Loading...</h2>
+				) : (
+					data.map((item) => (
+						<div className="row border-bottom py-3" key={item.id}>
+							<div className="col-3 d-flex justify-content-center align-self-center">
+								<img
+									className="thumbnail"
+									src={item.pic}
+									alt={item.firstName + " " + item.lastName}
+								/>
+							</div>
+							<div className="col-9">
+								<p className="name h1">
+									{item.firstName.toUpperCase()} {item.lastName.toUpperCase()}
+								</p>
+								<div className="detail row px-5">
+									<p>Email: {item.email}</p>
+									<p>Comapny: {item.company}</p>
+									<p>Skill: {item.skill}</p>
+									<p>
+										Grade:{" "}
+										{item.grades.reduce(
+											(a, b) => parseInt(a) + parseInt(b),
+											0
+										) / item.grades.length}
+										%
+									</p>
+								</div>
+							</div>
+						</div>
+					))
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
